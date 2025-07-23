03fc760ff3256961e2f73412df455d89b0771951
Chamodfernandoo <chamoddilshan0907@gmail.com>
2025-07-23 10:14:32 +0530

    test commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!devvvvvvvvvvvvvv</h3>
+      <h3>Welcome /</h3>
     </div>
   )
 }
